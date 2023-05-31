b12c4451651f7bfc567c40b9426475c3e574c72f
anastasiia1210 <anastasiia.lysa@ukma.edu.ua>
2023-05-31 20:52:54 +0300

    Added dev start script

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,7 +5,10 @@ module.exports = {
     username: process.env.TYPEORM_USERNAME,
     password: process.env.TYPEORM_PASSWORD,
     database: process.env.TYPEORM_DATABASE,
-    entities: ['dist/**/*.entity{.ts,.js}'],
+    entities: [
+        'dist/**/*.entity{.js,.ts}',
+        'src/Models/entity/*.entity.ts'
+    ],
     migrations: ['dist/migration/**/*.js'],
     charset: 'utf8mb8',
     cli: {
