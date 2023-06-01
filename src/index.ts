8c9cd5cbef2afac426593d2091ca140ecf025c14
natal <nataliia.malashok@ukma.edu.ua>
2023-06-01 22:26:40 +0300

    index.ts cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+//const express = require("express");
+const cors = require("cors");
 
 const port: number = +(process?.env?.PORT ?? 3001);
 
 (async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.use(cors({"Access-Control-Allow-Origin":"http://localhost:5173"}));
     await app.listen(port);
 })();
